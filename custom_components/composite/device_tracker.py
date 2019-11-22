55e0bfd80f93897a2f5b283838702a353a70284e
Phil <pnbruckner@gmail.com>
2019-11-22 13:31:03 -0600

    Bump version to 1.10.1b1

--- custom_components/composite/device_tracker.py
+++ custom_components/composite/device_tracker.py
@@ -28,7 +28,7 @@ from homeassistant.util.location import distance
 
 _LOGGER = logging.getLogger(__name__)
 
-__version__ = '1.10.0'
+__version__ = '1.10.1b1'
 
 CONF_TIME_AS = 'time_as'
 CONF_REQ_MOVEMENT = 'require_movement'
