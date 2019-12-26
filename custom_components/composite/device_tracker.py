0dd402b7574bc8811bea9c57c751bfe512652f83
Phil Bruckner <pnbruckner@gmail.com>
2019-12-26 10:25:40 -0600

    Remove beta suffix

--- custom_components/composite/device_tracker.py
+++ custom_components/composite/device_tracker.py
@@ -28,7 +28,7 @@ from homeassistant.util.location import distance
 
 _LOGGER = logging.getLogger(__name__)
 
-__version__ = '1.11.0b1'
+__version__ = '1.11.0'
 
 CONF_TIME_AS = 'time_as'
 CONF_REQ_MOVEMENT = 'require_movement'
