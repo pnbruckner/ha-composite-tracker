f2579d04e3d7ad667f18d7700cf988f4f1ba7b73
Phil Bruckner <pnbruckner@gmail.com>
2024-01-26 21:29:43 -0600

    Fix speed sensor name
    Was using a feature that will be new in 2024.2.

--- custom_components/composite/sensor.py
+++ custom_components/composite/sensor.py
@@ -49,16 +49,20 @@ class CompositeSensor(SensorEntity):
             self.entity_id = f"{S_DOMAIN}.{obj_id}"
             self._attr_unique_id = obj_id
         else:
+            # translation_placeholders was new in 2024.2
+            self._use_name_translation = hasattr(self, "translation_placeholders")
             entity_description = SensorEntityDescription(
                 key="speed",
                 device_class=SensorDeviceClass.SPEED,
                 icon="mdi:car-speed-limiter",
-                has_entity_name=True,
+                has_entity_name=self._use_name_translation,
                 translation_key="speed",
                 native_unit_of_measurement=UnitOfSpeed.METERS_PER_SECOND,
                 state_class=SensorStateClass.MEASUREMENT,
             )
             self._attr_translation_placeholders = {"name": entry.title}
+            if not self._use_name_translation:
+                self._attr_name = f"{entry.title} speed"
             self._attr_unique_id = signal = entry.entry_id
         self.entity_description = entity_description
         self._attr_extra_state_attributes = {
@@ -116,9 +120,11 @@ class CompositeSensor(SensorEntity):
             # Need to change _attr_translation_placeholders (instead of the dict to
             # which it refers) to clear the cached_property in new HA versions.
             self._attr_translation_placeholders = {"name": new_name}
-            # Clear cached_property in new HA versions.
-            self._attr_name = ""
-            del self._attr_name
+            self._attr_name = f"{new_name} speed"
+            if self._use_name_translation:
+                # Delete _attr_name so translated name is used.
+                # This also clears the cached_property in new HA versions.
+                del self._attr_name
             er.async_get(hass).async_update_entity(
                 self.entity_id, original_name=self.name
             )
