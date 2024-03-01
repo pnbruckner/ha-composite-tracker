4f8ee485f2559c128830f295d45598515e5db68c
Phil Bruckner <pnbruckner@gmail.com>
2024-03-01 09:27:11 -0600

    Add more error handling to local file processing

--- custom_components/composite/const.py
+++ custom_components/composite/const.py
@@ -1,6 +1,13 @@
 """Constants for Composite Integration."""
 DOMAIN = "composite"
 
+PICTURE_SUFFIXES = ("bmp", "jpg", "png")
+
+DEF_REQ_MOVEMENT = False
+
+MIN_SPEED_SECONDS = 3
+MIN_ANGLE_SPEED = 1  # meters / second
+
 SIG_COMPOSITE_SPEED = "composite_speed"
 
 CONF_ALL_STATES = "all_states"
@@ -12,11 +19,6 @@ CONF_REQ_MOVEMENT = "require_movement"
 CONF_TIME_AS = "time_as"
 CONF_TRACKERS = "trackers"
 CONF_USE_PICTURE = "use_picture"
-
-DEF_REQ_MOVEMENT = False
-
-MIN_SPEED_SECONDS = 3
-MIN_ANGLE_SPEED = 1  # meters / second
 
 ATTR_ACC = "acc"
 ATTR_ANGLE = "angle"
